faf3f5fb037967f53b4207f378e606cada1d7145
Kevin <kevin.pfeil@knights.ucf.edu>
2022-01-18 14:50:09 -0500

    working video stuff?

--- PeerConn/js/client.js
+++ PeerConn/js/client.js
@@ -6,12 +6,29 @@ var loginInput = document.querySelector('#loginInput');
 var loginBtn = document.querySelector('#loginBtn'); 
 var otherUsernameInput = document.querySelector('#otherUsernameInput'); 
 var connectToOtherUsernameBtn = document.querySelector('#connectToOtherUsernameBtn'); 
-var connectedUser, myConnection;
+var callPage = document.querySelector('#callPage');
+var loginPage = document.querySelector('#loginPage');
+var videoPage = document.querySelector('#videoPage');
+var myVideo = document.querySelector('#myVideo');
+var remoteVideo = document.querySelector('#remoteVideo');
+var yourName = document.querySelector('#userLogin');
+
+var connectedUser, myConnection, theStream;
+  
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};  
+  
+  
+//const servers = {'iceServers': [{'urls': 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'}]};
+  
+  
+  
   
 //when a user clicks the login button 
 loginBtn.addEventListener("click", function(event){ 
 
-	console.log("Dude");
    name = loginInput.value; 
 	
    if(name.length > 0){ 
@@ -19,37 +36,111 @@ loginBtn.addEventListener("click", function(event){
          type: "login", 
          name: name 
       }); 
-   } 
+   }
+   
+   callPage.style.display = "inline";
+   loginPage.style.display = "none";
+   userLogin.innerHTML = loginInput.value;
 	
 });
+
+
+
+
+//setup a peer connection with another user 
+connectToOtherUsernameBtn.addEventListener("click", function () {
+  
+	console.log("The call button was clicked");
+	callPage.style.display = "none";
+	videoPage.style.display = "inline";
+	
+	//initVideo();
+  
+  
+   var otherUsername = otherUsernameInput.value;
+   connectedUser = otherUsername;
+	
+   if (otherUsername.length > 0) { 
+      //make an offer 
+      myConnection.createOffer(function (offer) { 
+         console.log(); 
+			
+         send({ 
+            type: "offer", 
+            offer: offer 
+         }); 
+			
+         myConnection.setLocalDescription(offer); 
+		 console.log(myConnection);
+      }, function (error) { 
+         alert("An error has occurred."); 
+      }); 
+   } 
+});
+
+async function initVideo(){
+	
+	var stream1 = await navigator.mediaDevices.getUserMedia(constraints);
+	showOwnVideo(stream1);
+}
+  
+  
+function showOwnVideo(stream) {
+  const video = myVideo;
+  
+  const videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log(`Using video device: ${videoTracks[0].label}`);
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+}
+  
+
+function showRemoteVideo(stream) {
+	//todo... when the other user shows their video
+}
+  
+  
+  
+  
   
 //handle messages from the server 
 connection.onmessage = function (message) { 
-   console.log("Got message", message.data);
+   //console.log("Got message", message.data);
    
    try{
-   var data = JSON.parse(message.data); 
+		var data = JSON.parse(message.data); 
 	
-   switch(data.type) { 
-      case "login": 
-         onLogin(data.success); 
-         break; 
-      case "offer": 
-         onOffer(data.offer, data.name); 
-         break; 
-      case "answer": 
-         onAnswer(data.answer); 
-         break; 
-      case "candidate": 
-         onCandidate(data.candidate); 
-         break; 
-      default: 
-         break; 
+		switch(data.type) { 
+		case "login": 
+			onLogin(data.success); 
+			break; 
+		case "offer": 
+			onOffer(data.offer, data.name); 
+			break; 
+		case "answer": 
+			onAnswer(data.answer); 
+			break; 
+		case "candidate": 
+			onCandidate(data.candidate); 
+			break; 
+		default: 
+			break; 
+		}
    }
-   }
-   catch(error){};
+   catch(error){
+	   //console.log("Got something not JSON");
+   };
 };
-  
+
+
+
+
+
+
+
+
+
 //when a user logs in 
 function onLogin(success) { 
 
@@ -58,35 +149,70 @@ function onLogin(success) {
    } else { 
       //creating our RTCPeerConnection object 
 		
-      var configuration = { 
-         "iceServers": [{ "url": "stun:stun.1.google.com:19302" }] 
-      }; 
+      //********************** 
+      //Starting a peer connection 
+      //********************** 
 		
-      myConnection = new webkitRTCPeerConnection(configuration); 
-      console.log("RTCPeerConnection object was created"); 
-      console.log(myConnection); 
-  
-      //setup ice handling
-      //when the browser finds an ice candidate we send it to another peer 
-      myConnection.onicecandidate = function (event) { 
-		
-         if (event.candidate) { 
-            send({ 
-               type: "candidate", 
-               candidate: event.candidate 
-            }); 
-         } 
-      }; 
+      //getting local video stream 
+      navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) { 
+         theStream = myStream; 
+			
+         //displaying local video stream on the page 
+         myVideo.srcObject = theStream;
+			
+         //using Google public stun server 
+         var configuration = { 
+            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+         }; 
+			
+         myConnection = new RTCPeerConnection(configuration); 
+			
+         // setup stream listening 
+         myConnection.addStream(theStream); 
+			
+         //when a remote user adds stream to the peer connection, we display it 
+         myConnection.onaddstream = function (e) { 
+            remoteVideo.srcObject = e.stream;
+         };
+			
+         // Setup ice handling 
+         myConnection.onicecandidate = function (event) { 
+            if (event.candidate) { 
+               send({ 
+                  type: "candidate", 
+                  candidate: event.candidate 
+               }); 
+            } 
+         };  
+			
+      }, function (error) { 
+         console.log(error); 
+      }); 
    } 
 };
   
+  
+  
+  
+  
+  
+  
 connection.onopen = function () { 
-   console.log("Connected"); 
+   console.log("Connected to the signalling server"); 
 };
+  
+  
+  
+  
+  
   
 connection.onerror = function (err) { 
    console.log("Got error", err); 
 };
+  
+  
+  
+  
   
 // Alias for sending messages in JSON format 
 function send(message) { 
@@ -109,28 +235,7 @@ function send(message) {
 
 
 
-//setup a peer connection with another user 
-connectToOtherUsernameBtn.addEventListener("click", function () {
-  
-   var otherUsername = otherUsernameInput.value;
-   connectedUser = otherUsername;
-	
-   if (otherUsername.length > 0) { 
-      //make an offer 
-      myConnection.createOffer(function (offer) { 
-         console.log(); 
-			
-         send({ 
-            type: "offer", 
-            offer: offer 
-         }); 
-			
-         myConnection.setLocalDescription(offer); 
-      }, function (error) { 
-         alert("An error has occurred."); 
-      }); 
-   } 
-});
+
   
 //when somebody wants to call us 
 function onOffer(offer, name) { 
@@ -148,16 +253,26 @@ function onOffer(offer, name) {
    }, function (error) { 
       alert("oops...error"); 
    }); 
+   
+   console.log("Got an Offer from " + name);
 }
+
+
+
 
 //when another user answers to our offer 
 function onAnswer(answer) { 
    myConnection.setRemoteDescription(new RTCSessionDescription(answer)); 
+   console.log("Got an Answer");
 }
+  
+  
+  
   
 //when we got ice candidate from another user 
 function onCandidate(candidate) { 
    myConnection.addIceCandidate(new RTCIceCandidate(candidate)); 
+   console.log("Got an ICE Candidate");
 }
 
 
@@ -192,10 +307,3 @@ function openDataChannel() {
       console.log("Got message:", event.data); 
    };  
 }
-  
-//when a user clicks the send message button 
-sendMsgBtn.addEventListener("click", function (event) { 
-   console.log("send message");
-   var val = msgInput.value; 
-   dataChannel.send(val); 
-});
